aeae53c4597bdfdf622a588c64035b50bb577e5f
jhamilcali <jhamilquea@gmail.com>
2025-08-12 20:35:10 -0400

    Perfil de usuario y historial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/NavBar';
 import Home from './pages/Home';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<UserProfile />} />
             <Route path="*" element={
               <div className="text-center py-20">
                 <h1 className="text-4xl font-bold mb-4">404</h1>
